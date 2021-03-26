f07508fa79192acad76dd93d6b54ba3cb3ff243b
schneider001 <69435543+schneider001@users.noreply.github.com>
2021-03-26 15:12:01 +0300

    Add files via upload

--- klava.ino
+++ klava.ino
@@ -1,54 +1,37 @@
-#define ON_PRESS 1
-#define BUZZER_PIN 6
+#define ON_PRESS        1
+#define BUZZER_PIN      6
 
-class Piano
+class AmperkaKB
 {
 public:
-    Piano(uint8_t = 255, uint8_t = 255, uint8_t = 255, uint8_t = 255, uint8_t = 255, uint8_t = 255, uint8_t = 255);
-
+    AmperkaKB(uint8_t = 255, uint8_t = 255, uint8_t = 255, uint8_t = 255, uint8_t = 255, uint8_t = 255, uint8_t = 255);
+   
     void read();
 
     void begin();
-
+    
     bool justPressed();
-
-    void setColor(int key);
-
+ 
     uint8_t getNum = 0;
-
+  
     char getChar = 0;
-
 private:
     uint8_t _pinsKB[7];
-
-    uint8_t _redPin = 2;
-    uint8_t _greenPin = 4;
-    uint8_t _bluePin = 5;
-
-    enum _color
-    {
-        BLACK = 0,
-        RED,
-        YELLOW,
-        GREEN,
-        LIGHT_BLUE,
-        BLUE,
-        PURPLE
-    };
-
+    
     uint8_t _state;
-
+    
     uint8_t _numberKeyNow;
-
+    
     uint8_t _massNumberKey[16] = {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xE, 0x0, 0xF, 0x0, 0x0, 0x0, 0x0};
 
     char _massCharKey[16] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#', '0', '0', '0', '0'};
+       
 
     void _findPressKeyNow();
 };
 
-Piano::Piano(uint8_t pin1KB, uint8_t pin2KB, uint8_t pin3KB, uint8_t pin4KB, uint8_t pin5KB, uint8_t pin6KB, uint8_t pin7KB)
-{
+
+AmperkaKB::AmperkaKB(uint8_t pin1KB, uint8_t pin2KB, uint8_t pin3KB, uint8_t pin4KB, uint8_t pin5KB, uint8_t pin6KB, uint8_t pin7KB) {
     _pinsKB[0] = pin1KB;
     _pinsKB[1] = pin2KB;
     _pinsKB[2] = pin3KB;
@@ -58,151 +41,82 @@ Piano::Piano(uint8_t pin1KB, uint8_t pin2KB, uint8_t pin3KB, uint8_t pin4KB, uin
     _pinsKB[6] = pin7KB;
 }
 
-void Piano::begin()
-{
-    pinMode(_redPin, OUTPUT);
-    pinMode(_greenPin, OUTPUT);
-    pinMode(_bluePin, OUTPUT);
 
-    pinMode(_pinsKB[0], OUTPUT);
-    pinMode(_pinsKB[1], OUTPUT);
-    pinMode(_pinsKB[2], OUTPUT);
-    pinMode(_pinsKB[3], OUTPUT);
-    digitalWrite(_pinsKB[0], HIGH);
-    digitalWrite(_pinsKB[1], HIGH);
-    digitalWrite(_pinsKB[2], HIGH);
-    digitalWrite(_pinsKB[3], HIGH);
-
-    pinMode(_pinsKB[4], INPUT_PULLUP);
-    pinMode(_pinsKB[5], INPUT_PULLUP);
-    pinMode(_pinsKB[6], INPUT_PULLUP);
+void AmperkaKB::begin() {
+  
+     pinMode(_pinsKB[0], OUTPUT);
+     pinMode(_pinsKB[1], OUTPUT);
+     pinMode(_pinsKB[2], OUTPUT);
+     pinMode(_pinsKB[3], OUTPUT);
+     digitalWrite(_pinsKB[0], HIGH);
+     digitalWrite(_pinsKB[1], HIGH);
+     digitalWrite(_pinsKB[2], HIGH);
+     digitalWrite(_pinsKB[3], HIGH);
+          
+     pinMode(_pinsKB[4], INPUT_PULLUP);
+     pinMode(_pinsKB[5], INPUT_PULLUP);
+     pinMode(_pinsKB[6], INPUT_PULLUP);
+        
 }
 
-void Piano::read()
-{
-    _state = false;
 
+void AmperkaKB::read() {
+    _state = false;
+    
     _findPressKeyNow();
-    if (_numberKeyNow != 255)
-    {
-        getNum = _massNumberKey[_numberKeyNow];
+    if (_numberKeyNow != 255) {
+        getNum =  _massNumberKey[_numberKeyNow];
         getChar = _massCharKey[_numberKeyNow];
         _state = ON_PRESS;
     }
 }
 
-bool Piano::justPressed()
-{
+bool AmperkaKB::justPressed() {
     return _state == ON_PRESS ? 1 : 0;
 }
 
-void Piano::_findPressKeyNow()
-{
+void AmperkaKB::_findPressKeyNow() {
     _numberKeyNow = 255;
-
-    for (uint8_t i = 0; i < 4; i++)
-    {
-
+   
+    for (uint8_t i = 0; i < 4; i++) {
+                
         digitalWrite(_pinsKB[i], LOW);
-        for (uint8_t j = 0; j < 3; j++)
-        {
-
-            if (!digitalRead(_pinsKB[j + 4]))
-            {
+        for (uint8_t j = 0; j < 3; j++) {
+                    
+            if (!digitalRead(_pinsKB[j + 4])) {
                 _numberKeyNow = i * 3 + j;
             }
         }
-
+        
         digitalWrite(_pinsKB[i], HIGH);
     }
 }
 
-void Piano::setColor(int key)
-{
-    int colorVar = (int)_color::BLACK;
 
-    if (key != (int)_color::BLACK)
-        colorVar = key % 6;
+AmperkaKB KB(13, 12, 11, 10, 9, 8, 7);
 
-    int red = 0;
-    int green = 0;
-    int blue = 0;
-
-    switch (colorVar)
-    {
-    case _color::RED:
-        red = 255;
-        break;
-    case _color::YELLOW:
-        red = 255;
-        green = 255;
-        break;
-    case _color::GREEN:
-        green = 255;
-        break;
-    case _color::LIGHT_BLUE:
-        green = 255;
-        blue = 255;
-        break;
-    case _color::BLUE:
-        blue = 255;
-        break;
-    case _color::PURPLE:
-        red = 80;
-        blue = 80;
-        break;
-    case _color::BLACK:
-        red = 0;
-        green = 0;
-        blue = 0;
-        break;
-
-    default:
-        break;
-    }
-
-    analogWrite(redPin, red);
-    analogWrite(greenPin, green);
-    analogWrite(bluePin, blue);
+ 
+void setup() {
+  Serial.begin(9600);
+  KB.begin();
+  pinMode(BUZZER_PIN, OUTPUT);
 }
-
-Piano piano(13, 12, 11, 10, 9, 8, 7);
-
-void setup()
-{
-    Serial.begin(9600);
-    piano.begin();
-    pinMode(BUZZER_PIN, OUTPUT);
-}
-
-void loop()
-{
+ 
+void loop() {
     int frequency = 475.73 / 4;
-
-    piano.read();
-
-    if (piano.justPressed())
-    {
-        int i = piano.getNum;
-        if (i == 0)
-        {
+    KB.read();
+    if (KB.justPressed()) {
+        int i = KB.getNum;
+        if (i == 0) {
             i = 11;
         }
-        if (i == 14)
-        {
+        if (i == 14) {
             i = 10;
         }
-        if (i == 15)
-        {
+        if (i == 15) {
             i = 12;
         }
-
         Serial.println(i);
-        setColor(i);
         tone(BUZZER_PIN, frequency * pow(1.0595, i), 10);
-    }
-    else
-    {
-        setColor((int)_color::BLACK);
     }
 }
